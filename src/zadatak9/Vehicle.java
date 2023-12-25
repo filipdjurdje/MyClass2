2e819dcbef9c96949764e49b3a47097bf6637a52
unknown <filipdjurdjevac12356@gmail.com>
2023-12-25 07:01:33 +0100

    first commit

---
+++ src/zadatak9/Vehicle.java
@@ -0,0 +1,44 @@
+package zadatak9;
+
+public class Vehicle {
+
+	private String color, New;
+	private int x, y;
+
+
+
+	public String getNew() {
+		return New;
+	}
+
+	public void setNew(String red) {
+		this.New = red;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+
+
+}
